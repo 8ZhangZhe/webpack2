2e9d3369fc4cd4d747a114c117c85c35a4c02079
zhangzhe <447567191@qq.com>
2022-06-17 23:38:31 +0800

    完成了css加载功能

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import { marquee } from './marquee.js'
 import { tab } from './tab.js'
+marquee()
+tab()
+
+import './style/index.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
-const { join } = require("path")
+const { join } = require("path");
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/main.js", // 入口
-    output: { 
-        path: join(__dirname, "lib"), // 出口路径
-        filename: "webpack-dome.js", // 出口文件名
-        clean: true
-    }
-}
+  mode: "development",
+  entry: "./src/main.js", // 入口
+  output: {
+    path: join(__dirname, "lib"), // 出口路径
+    filename: "webpack-dome.js", // 出口文件名
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 建议是绝对路径
+      template: join(__dirname, "public/index.html"),
+    }),
+  ],
+  devServer: {
+    port: 30000, // 端口号
+    open: true
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: [ "style-loader", "css-loader"]
+        }
+    ]
+  }
+};
